5b10af8ca479e3f3db6a9162f7ba5308bb4c8ce5
Harshit <harshitmohan16@gmail.com>
2024-09-26 22:18:16 +0530

    complete sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import { useState, useCallback } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+// import { useDispatch, useSelector } from 'react-redux'
+// import {
+//   signInStart,
+//   signInSuccess,
+//   signInFailure,
+// } from '../redux/user/userSlice'
+// import OAuth from '../components/OAuth'
 
-function SignIn() {
+const initalDetail = {
+  email: '',
+  password: ''
+  }
+
+export default function SignIn() {
+  const [userDetail, setUserDetail] = useState(Object.assign({}, initalDetail))
+  // const { loading, error } = useSelector((state) => state.user)
+  const navigate = useNavigate()
+  // const dispatch = useDispatch()
+
+ const handleChangeDetails = useCallback((key, value) => {
+    const updateDetail = Object.assign({}, userDetail)
+    updateDetail[key] = value
+    setUserDetail(updateDetail)
+  }, [userDetail])
+
+  const handleSubmit = useCallback( async(e) => {
+    e.preventDefault()
+    try {
+      // dispatch(signInStart())
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userDetail),
+      })
+      const data = await res.json()
+
+      if (data.success === false) {
+        // dispatch(signInFailure(data.message))
+        return
+      }
+      // dispatch(signInSuccess(data))
+      navigate('/')
+    } catch (error) {
+      // dispatch(signInFailure(error.message))
+    }
+  }, [userDetail])
+
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={(e) => handleChangeDetails('email', e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={(e) => handleChangeDetails('password', e.target.value)}
+        />
+
+        <button
+          // disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {/* {loading ? 'Loading...' : 'Sign In'} */}
+        </button>
+        {/* <OAuth/> */}
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+    </div>
   )
 }
-
-export default SignIn
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useCallback} from 'react'
-
 import { Link, useNavigate } from 'react-router-dom'
 
 const initalDetail = {

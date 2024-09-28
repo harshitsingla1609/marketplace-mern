442f98b1787278c9c6da0679eadfca6cc3bc82ef
Harshit <harshitmohan16@gmail.com>
2024-09-28 16:06:48 +0530

    Profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,6 @@ import { useSelector } from 'react-redux';
 
 function Header() {
 	const { currentUser } = useSelector((state) => state.user);
-	console.log(currentUser, 'currentUser');
 	return (
 		<header className="bg-slate-200 shadow-md">
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">

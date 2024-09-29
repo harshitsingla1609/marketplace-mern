d5a0425c865b8b7d7539480147f755f25501e402
Harshit <harshitmohan16@gmail.com>
2024-09-29 21:53:28 +0530

    complete listing page UI with functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,8 @@ const App = () => {
 				<Route path="/sign-in" element={<SignIn />} />
 				<Route path="/sign-up" element={<SignUp />} />
 				<Route path="/about" element={<About />} />
+				<Route path='/listing/:listingId' element={<Listing />} />
+
 				<Route element={<PrivtaeRoute />}>
 					<Route path="/profile" element={<Profile />} />
 					<Route path='/create-listing' element={<CreateListing />} />

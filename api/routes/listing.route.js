1237bba71bd1082013d78dd9704c8b02fc97091a
Harshit <harshitmohan16@gmail.com>
2024-09-29 20:24:28 +0530

    complete delete and show listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -6,6 +6,6 @@ import { verifyToken } from '../utils/verifyUser.js'
 const router = express.Router()
 
 router.post('/create', verifyToken, createListing)
-router.post('/delete', verifyToken, deleteListing)
+router.delete('/delete/:id', verifyToken, deleteListing)
 
 export default router 
